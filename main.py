184a9788e2478e47678719f02187de6a0cbb716a
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-10 15:51:40 -0500

    created form

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask 
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = True # a watchdog like inotify reloader
+
+form = """
+<!doctype html>
+<html>
+    <body>
+        <form>
+            <label for ="first_name">First Name:</label>
+            <input type = "text" name="first_name" />
+            <input type ="submit" />
+        </form>
+    </body>
+</html>
+
+"""
+
+
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
 
 app.run()
+
+# @app.route("/test_route", methods=["GET", "POST"])
+
+# def index():
+#     if request.type == "GET": # Ok I couldn't keep up taking 
+#         # notes in class 
+#         return "Hello World"
+#     elif request.type == "POST":
+#         return "Request was a post"
+    
+#     return "Method not found"
+
