4dfb8507e74a7026c004ac417b520fc6884f4885
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-12 16:06:44 -0500

    creates server-side validation form

--- main.py
+++ main.py
@@ -28,6 +28,23 @@ def hello():
     first_name = request.form['first_name']
     return '<h1>Hello, ' + first_name + '</h1>'
 
+time_form = """
+<style>
+    .error {{ color: red; }}
+</style>
+<h1>Validate Time</h1>
+<form method='POST'>
+    <label>Hours (24-hour format)
+        <input name="hours" type="text" value='{hours}' />
+    </label>
+    p class="error">{hours_error}</p>
+    <label>Minutes
+        <input name="minutes" type="text" value='{minutes}' />
+    </label>
+    p class="error">{minutes_error}</p>
+        <input type="submit" value="Validate" />
+</form>
+"""
 app.run()
 
 # @app.route("/test_route", methods=["GET", "POST"])
