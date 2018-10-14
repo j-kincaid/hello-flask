f35d2265613b412442e12c4b0d53fc492438094b
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-14 18:11:48 -0500

    removed name field

--- main.py
+++ main.py
@@ -23,11 +23,6 @@ form = """
 def index():
     return form
 
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name = request.form['first_name']
-    return '<h1>Hello, ' + first_name + '</h1>'
-
 time_form = """
 <style>
     .error {{ color: red; }}
