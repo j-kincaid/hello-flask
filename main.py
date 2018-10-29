c50956aedbebee6e396891955a41a18289f662d7
Jessica Kincaid <4j.kincaid@gmail.com>
2018-10-29 11:10:57 -0500

    changed to render_template format

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
 import os
 import jinja2
@@ -16,9 +16,7 @@ def index():
 
 @app.route("/hello", methods=['POST'])
 def hello():
-    first_name = request.form['first_name']
-    template = jinja_env.get_template('hello_greeting.html')
-    return template.render(name=first_name)
+    return render_template('hello_form.html')
 
 
 @app.route('/validate-time')
